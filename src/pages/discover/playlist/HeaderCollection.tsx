b8cbed7e9aa2a8620cdf5a1f062b903cc1b2c35d
TruongLe <leminhtruong0105@gmail.com>
2023-02-12 17:31:19 +0700

    up

--- src/pages/discover/playlist/HeaderCollection.tsx
+++ src/pages/discover/playlist/HeaderCollection.tsx
@@ -1,33 +1,71 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState, useEffect } from 'react';
 
 import { categories, mood, scene, topic } from '../../../jsons/collection';
 
 const HeaderCollection = () => {
-    const Tag = ({
-        data,
-        collection
-    }: any) => {
+    const [showMore, setShowMore] = useState({
+        title: '',
+        data: [],
+    });
+
+    const MoreCollection = () => {
+
         return (
-            <div>
-                <h3>{collection}</h3>
-                <div className='flex flex-col'>
-                    {data.map((el: string, index: number) => (
-                        <div key={index} style={{ marginBottom: '5px' }} className='flex align-center'>
-                            <i className="fa-solid fa-angle-right"></i>
-                            <a title={el}>{el}</a>
-                        </div>
-                    ))}
-                    <div className='flex align-center'>
+            <div className={
+                showMore.title === 'Khung cảnh' ?
+                    ` more-collection scene-collection` :
+                    showMore.title === 'Chủ đề' ?
+                        ' more-collection orther-collection' :
+                        ' more-collection'
+            }
+            >
+                {showMore?.data?.slice(5,).map((el: string, index: number) => (
+                    <div key={index} className='flex align-center'>
                         <i className="fa-solid fa-angle-right"></i>
-                        <a>Xem thêm...</a>
+                        <a className='pointer name-song cl-blur' title={el}>{el}</a>
                     </div>
-                </div>
+                ))}
             </div>
         )
     };
 
+    const Tag = ({
+        data,
+        collection
+    }: any) => {
+
+        const handleClickMore = (collection: string, data: []) => {
+            setShowMore({
+                title: collection,
+                data: data
+            })
+        }
+
+        return (
+            <div className='collection-item'>
+                <h3>{collection}</h3>
+                <div className='flex flex-col'>
+                    {data.slice(0, 5).map((el: string, index: number) => (
+                        <div key={index} style={{ marginBottom: '5px' }} className='flex align-center'>
+                            <i className="fa-solid fa-angle-right"></i>
+                            <a className='pointer name-song cl-blur' title={el}>{el}</a>
+                        </div>
+                    ))}
+                    <div
+                        onClick={() => handleClickMore(collection, data)}
+                        className='flex align-center'
+                    >
+                        <i className="fa-solid fa-angle-right"></i>
+                        <a className='pointer'>Xem thêm...</a>
+                    </div>
+                </div>
+            </div >
+        )
+    };
+
     return (
-        <div className='flex justify-between header-collection'>
+        <div className='flex justify-between relative header-collection'>
             <Tag
                 data={categories}
                 collection='Thể loại'
@@ -44,7 +82,11 @@ const HeaderCollection = () => {
                 data={topic}
                 collection='Chủ đề'
             />
-
+            {
+                showMore.title && (
+                    <MoreCollection />
+                )
+            }
         </div>
     )
 }
